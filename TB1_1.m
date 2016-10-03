7a4e4d992604f8912f894c03d30d63a7e491f97b
Junho Kim <kuh9325@naver.com>
2016-10-03 12:54:56 +0900

    modifying that it doesn't need input variable

--- TB1_1.m
+++ TB1_1.m
@@ -1,7 +1,5 @@
 %% based on txtbk [Engineering Vibration, 4th Ed, Inman], chp1 toolbox 1.1 %%
 
-function TB1_1(v0)
-
 % initial condition setting
 
 m = 49.2*10^-3; % unit:kg
@@ -9,8 +7,11 @@ c = 0.11; % unit:kg/s
 k = 857.8; % unit:N/m
 x0 = 0.001; % unit:m
 % v0 is input variable
-tf = 10;
+tf = 5;
 
-vtb1_1(m,c,k,x0,v0,tf)
+vtb1_1_mod(m,c,k,x0,0,tf)
+vtb1_1_mod(m,c,k,x0,0.1,tf)
+vtb1_1_mod(m,c,k,x0,1,tf)
+vtb1_1_mod(m,c,k,x0,2,tf)
 
 legend('v(0) = 0','v(0) = 0.1','v(0) = 1','v(0) = 2')
--- TB1_2.m
+++ TB1_2.m
@@ -1,7 +1,5 @@
 %% based on txtbk [Engineering Vibration, 4th Ed, Inman], chp1 toolbox 1.2 %%
 
-function TB1_2(x0)
-
 % initial condition setting
 
 m = 49.2*10^-3; % unit:kg
@@ -9,8 +7,10 @@ c = 0.11; % unit:kg/s
 k = 857.8; % unit:N/m
 % x0 is input variable
 v0 = 0; % unit:m/s
-tf = 3.5;
+tf = 5;
 
-vtb1_1(m,c,k,x0,v0,tf)
+vtb1_1_mod(m,c,k,0.001,v0,tf)
+vtb1_1_mod(m,c,k,0.002,v0,tf)
+vtb1_1_mod(m,c,k,0.005,v0,tf)
 
 legend('x(0) = 0.001','x(0) = 0.002','x(0) = 0.005')
